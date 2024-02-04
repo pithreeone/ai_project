3a5d3bbd7cae1d5595f0f0c483cd14dd4a6d7f6b
pithreeone <pithreeone@gmail.com>
2024-02-04 11:12:47 +0800

    Modify: dataLoader.cpp

--- cpp/src/dataLoader.cpp
+++ cpp/src/dataLoader.cpp
@@ -17,12 +17,20 @@ void DataLoader::loadDataFromFolder(){
             // Get the path of the file
             std::string filePath = entry.path().string();
             // Read the image
-            cv::Mat image = cv::imread(filePath, cv::IMREAD_COLOR);
+            cv::Mat image = cv::imread(filePath, cv::IMREAD_GRAYSCALE);
             if (!image.empty()) {
                 // Successfully read the image, now you can process it
                 // For example, display the image
-                cv::imshow("Image", image);
+                // cv::imshow("Image", image);
                 cv::waitKey(0); // Wait for a key press
+                Eigen::MatrixXd eigenImage(image.rows, image.cols);
+                for(int i = 0; i < image.rows; ++i) {
+                    for(int j = 0; j < image.cols; ++j) {
+                        eigenImage(i, j) = image.at<uchar>(i, j);
+                    }
+                }
+                train_x_.push_back(eigenImage);
+                train_y_.push_back();
             } else {
                 std::cerr << "Failed to read image: " << filePath << std::endl;
             }
