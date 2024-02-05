f7a4be1141b6fa30e4596774d08663eb69a02a06
fionalin0221 <fionalin0221@gmail.com>
2024-02-05 02:41:53 -0500

    Feat:add randomIntialize in kernel.cpp

--- cpp/src/kernel.cpp
+++ cpp/src/kernel.cpp
@@ -1,5 +1,8 @@
 #include "kernel.h"
-
+#include <iostream>
+#include <Eigen/Dense>
+using namespace Eigen;
+using namespace std;
 
 Kernel3d::Kernel3d(){
 }
@@ -21,5 +24,23 @@ Kernel3d::Kernel3d(int kernel_size, int channels){
 }
 
 void Kernel3d::randomIntialize(double min, double max){
-
+    double range = max-min;
+    for (auto i=kernel_.begin(); i!=kernel_.end(); i++){
+        MatrixXf m = MatrixXf::Random(kernel_size_, kernel_size_);
+        m = (m + MatrixXf::Constant(kernel_size_,kernel_size_,1.))*range/2.;
+        *i = (m + MatrixXf::Constant(kernel_size_,kernel_size_,min));
+    }
+    for (auto i=kernel_derivative_.begin(); i!=kernel_derivative_.end(); i++){
+        MatrixXf m = MatrixXf::Random(kernel_size_, kernel_size_);
+        m = (m + MatrixXf::Constant(kernel_size_,kernel_size_,1.))*range/2.;
+        *i = (m + MatrixXf::Constant(kernel_size_,kernel_size_,min));
+    }
+    for (auto i=0; i!=channels_; i++){
+        double f = (double)rand() / RAND_MAX;
+        bias_ = min + f * range;
+    }
+    for (auto i=0; i!=channels_; i++){
+        double f = (double)rand() / RAND_MAX;
+        bias_derivative_ = min + f * range;
+    }
 }
