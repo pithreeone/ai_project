7ee61fa567b0466c07d86127dc6d78dbf10dc77d
pithreeone <pithreeone@gmail.com>
2024-02-07 14:24:39 +0800

    Develop: develop back-propagation

--- cpp/include/cnn.h
+++ cpp/include/cnn.h
@@ -12,6 +12,7 @@ public:
     
     bool network_is_legal_;
     std::vector<NN> network_;
+    std::vector<Eigen::MatrixXf> input_;
     Eigen::VectorXf output_;
 
     // Constructor: set the structure of neural network
@@ -20,13 +21,13 @@ public:
     // Print the network of CNN
     void print();
 
-    // check if network is legal, change the variable: network_is_legal_
+    // Check if network is legal, change the variable: network_is_legal_
     void checkNetwork();
 
-    // do the forward-propagation with just one input data
+    // Do the forward-propagation with just one input data
     void forward(Eigen::MatrixXf input, Eigen::VectorXf& output, int& y);
 
-    // do the forward-propagation with many input data
+    // Do the forward-propagation with many input data. Clear all the results before do the forward-propogation.
     void forward(std::vector<Eigen::MatrixXf> input, std::vector<Eigen::VectorXf>& output, std::vector<int>& y);
 
 };
